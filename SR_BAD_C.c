31faf32f428d398883b58f7c76ae42e01f0f690e
MascySylvan <48143085+MascySylvan@users.noreply.github.com>
2025-04-11 21:43:36 +0800

    Create SR_BAD_C.c

---
+++ SR_BAD_C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void generateReportAndSaveAndSend() {
+    char *report = "Report content";
+
+    // Save to file
+    printf("Saving to file: %s\n", report);
+
+    // Send email
+    printf("Sending email: %s\n", report);
+}
